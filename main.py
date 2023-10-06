59bb60997399a49911b5df83d25c8b0b9782a1fd
peter-kim-3d <131735523+peter-kim-3d@users.noreply.github.com>
2023-10-06 01:51:24 -0400

    Update main.py
    adding button for meaning

--- main.py
+++ main.py
@@ -14,15 +14,26 @@ count = st.slider('Number of words', min_value=1, max_value=50, value=10)
 
 def get_frequent_words(letter, count):
     """Return frequently used words starting with a given letter using langchain."""
-    # This is a mock-up. You'll need to replace this with an actual method call to langchain.
     response = chat_model.predict(f"Give me {count} frequently used words starting with the letter {letter}")
-    # Assuming the response is a list of words, otherwise you might need to process it
+    return response
+
+def get_word_definition(word):
+    """Fetch the definition of a word using langchain."""
+    # Mock-up; replace with actual method call
+    response = chat_model.predict(f"What is the definition of {word}?")
     return response
 
 if len(letter) == 1:
     if st.button('Generate Words'):
         with st.spinner('Fetching words...'):
             words = get_frequent_words(letter, count)
-            st.write(words)
+            
+            for word in words:
+                col1, col2 = st.beta_columns(2)
+                col1.write(word)
+                if col2.button(f"Meaning of {word}"):
+                    definition = get_word_definition(word)
+                    st.write(f"Definition of {word}: {definition}")
 else:
     st.write("Please input a single letter.")
+
